41444429c79599e0bb65965960ec18a067ee46a4
Shengyu Li <shspku365@gmail.com>
2021-06-03 20:03:39 +0800

    添加按钮

--- src/Ripple.js
+++ src/Ripple.js
@@ -1,4 +1,7 @@
 import { Componet } from 'react';
+import Transition from 'react-transition-group/Transition';
+
+import './ripple.css'
 
 class Ripple extends Componet {
   constructor(props) {
@@ -19,6 +22,42 @@ class Ripple extends Componet {
     this.setState({
       wrapperExiting: true
     })
+  }
+
+  render() {
+    const {
+      className,
+      rippleX,
+      rippleY,
+      rippleSize,
+      color,
+      timeout,
+      ...other
+    } = this.props
+
+    const { wrapperExiting, rippleEntering } = this.state;
+
+    return (
+      <Transition
+        onEnter={this.handleEnter}
+        onExit={this.handleExit}
+        timeout={timeout}
+        {...other}
+      >
+        <span className={wrapperExiting ? 'rtr-ripple-wrapper-exiting' : ''}>
+            <span 
+                className={rippleEntering ? 'rtr-ripple-entering' : ''}
+                style={{
+                    width: rippleSize,
+                    height: rippleSize,
+                    top: rippleY - (rippleSize / 2),
+                    left: rippleX - (rippleSize / 2),
+                    backgroundColor: color,
+                }} 
+            />
+        </span>
+      </Transition>
+    )
   }
 
 }
